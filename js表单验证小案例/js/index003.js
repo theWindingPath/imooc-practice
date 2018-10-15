10448055a031b3bc34faeba3a7ffbec83924055e
theWindingPath <2958202145@qq.com>
2018-10-15 08:16:27 +0800

    正则表达式小案例 -步骤3

--- js表单验证小案例/js/index003.js
+++ js表单验证小案例/js/index003.js
@@ -3,12 +3,20 @@ var userAccount = document.getElementById('userAccount');
 var userPass = document.getElementById('userPass');
 var userPass_ = document.getElementById('userPass_'); // 确认密码
 var userName = document.getElementById('userName');
+var information = document.getElementById('information');
+var email = document.getElementById('email');
+var telephone = document.getElementById('telephone');
+var handup = document.getElementById('handup'); // 提交按钮
+
 // alert(userAccount); // 测试 js文件是否引入成功
 
 var test1 = false; // 用户名称格式正确 true 默认为false
 var test2 = false;
 var test3 = false;
 var test4 = false;
+var test5 = false;
+var test6 = false;
+var test7 = false;
 
 // 获取所有提示元素的下标
 var items = document.querySelectorAll('.item_'); // 定义一个数组 放提示信息
@@ -104,5 +112,91 @@ userName.onblur = function() {
             items[3].style.color = 'green';
             test4 = true;
         }
+    }
+}
+
+// 身份证验证
+information.onfocus = function () { // 光标落进去 提示
+    items[4].innerHTML = '请输入身份证号码';
+    items[4].style.color = 'darkgoldenrod';
+}
+
+information.onblur = function() {
+    // 二代身份证一般 18位 前17位位数字 后一位为数字或 x
+    // \d 匹配数字
+    var reg = /^\d{17}[0-9x]$/; // 局部变量
+    if(this.value == '') {
+        items[4].innerHTML = '身份证号不能为空';
+        items[4].style.color = 'red';
+    }else{ // 判断
+        if(!reg.exec(information.value)) { // 如果不匹配 
+            items[4].innerHTML = '请输入正确的身份证号';
+            items[4].style.color = 'red';
+        }else{ // 姓名正确
+            items[4].innerHTML = '身份证号正确';
+            items[4].style.color = 'green';
+            test5 = true;
+        }
+    }
+}
+
+// 邮箱验证
+email.onfocus = function () { // 光标落进去 提示
+    items[5].innerHTML = '请输入邮箱';
+    items[5].style.color = 'darkgoldenrod';
+}
+
+email.onblur = function() {
+    // 邮箱正则 @ . aa@aa.com 
+    // 前面aa 没有次数限制 至少有一次 用 +
+    // 加 .需要有 \反斜杠 转义 \. 代表.
+    // 后缀 字母、下划线 [a-zA-Z_] .com .cn .org 一般 2到4位 {2,4}
+    var reg = /^\w+@\w+\.[a-zA-Z_]{2,4}$/; // 局部变量 
+    if(this.value == '') {
+        items[5].innerHTML = '邮箱号不能为空';
+        items[5].style.color = 'red';
+    }else{ // 判断
+        if(!reg.exec(email.value)) { // 如果不匹配 
+            items[5].innerHTML = '请输入正确的邮箱号';
+            items[5].style.color = 'red';
+        }else{ // 姓名正确
+            items[5].innerHTML = '邮箱号正确';
+            items[5].style.color = 'green';
+            test6 = true;
+        }
+    }
+}
+
+// 手机验证
+telephone.onfocus = function () { // 光标落进去 提示
+    items[6].innerHTML = '请输入手机号';
+    items[6].style.color = 'darkgoldenrod';
+}
+
+telephone.onblur = function() {
+    // 手机正则 11位
+    var reg = /^\d{11}$/; // 局部变量 
+    if(this.value == '') {
+        items[6].innerHTML = '手机号不能为空';
+        items[6].style.color = 'red';
+    }else{ // 判断
+        if(!reg.exec(telephone.value)) { // 如果不匹配 
+            items[6].innerHTML = '请输入正确的手机号';
+            items[6].style.color = 'red';
+        }else{ // 姓名正确
+            items[6].innerHTML = '手机号正确';
+            items[6].style.color = 'green';
+            test7 = true;
+        }
+    }
+}
+
+// 提交按钮 注册的时候，必须前面所有数据都是正确的 才能提交
+handup.onclick = function() {
+    // alert(test1); 
+    if(test1 == false || test2 == false || test3 == false || test4 == false || test5 == false || test6 == false || test7 == false) {
+        alert('您填写的信息有误');
+    }else {
+        alert('提交成功');
     }
 }
